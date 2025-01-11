5c93896e23623c747ac7dea463681fba337eb1dd
nisaja26 <nissaikarachmawati@gmail.com>
2025-01-11 13:21:15 +0700

    Menambahkan navigasi roole

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -20,13 +22,17 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
     // permissions route
     Route::resource('/permissions', PermissionController::class);
 
+    // roles route
+    Route::resource('roles', RoleController::class)->except('show');
+    
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
-// Menambahkan route baru dengan nama permissions
+require __DIR__ . '/auth.php';
+// menambahkan sebuah route baru dengan nama roles
